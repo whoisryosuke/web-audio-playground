43660e37500b944ad5715a5d12e0868b2a2eb211
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-06-11 11:10:46 -0700

    :sparkles: static waveform

---
+++ src/components/Waveform/StaticWaveform.tsx
@@ -0,0 +1,27 @@
+import { LineGraph } from "@whoisryosuke/oat-milk-design";
+import mapRange from "../../utils/mapRange";
+
+type Props = {
+  buffer: AudioBuffer;
+};
+
+const StaticWaveform = ({ buffer, ...props }: Props) => {
+  // Get the waveform data
+  const waveformData = buffer.getChannelData(0);
+
+  console.log(
+    "waveform data",
+    // waveformData,
+    waveformData.filter((data) => data > 5)
+  );
+
+  const graph: number[] = [];
+
+  // Map the waveform data form -1 to 1 to the graphs Y axis
+  // Default is 0,1 for graph (lowest scale) - but we might scale up for zooming
+  waveformData.forEach((data) => graph.push(mapRange(data, -1, 1, -4, 5)));
+
+  return <LineGraph data={graph} color={"blue"} />;
+};
+
+export default StaticWaveform;
